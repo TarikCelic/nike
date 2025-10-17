3721dae2eaca351b36ac0115c3a2dd8c995324b4
Tarik Celic <tarikcelic5@gmail.com>
2025-10-17 09:12:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+
+// dom
 const rightArrowScroll = document.querySelector('.switches .switch:last-child')
 
 const leftArrowScroll = document.querySelector('.switches .switch:first-child')
 
+const shoesGrid = document.querySelector('#shoes')
+
+
+// scroll arrows
 leftArrowScroll.addEventListener('click', () => {
-  document.querySelector('#shoes').scrollBy({ left: -420, behavior: 'smooth' });
+  shoesGrid.scrollBy({ left: -420, behavior: 'smooth' });
 });
 
 rightArrowScroll.addEventListener('click', () => {
-  document.querySelector('#shoes').scrollBy({ left: 420, behavior: 'smooth' });
+  shoesGrid.scrollBy({ left: 420, behavior: 'smooth' });
 });
